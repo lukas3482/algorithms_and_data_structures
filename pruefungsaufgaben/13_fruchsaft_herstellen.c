1a0cc4ad4aedad82f378953c80196398af310e7e
Lukas Wolf <lukas.wolf00@gmx.at>
2025-03-25 19:42:54 +0100

    Create 13_fruchsaft_herstellen.c

---
+++ pruefungsaufgaben/13_fruchsaft_herstellen.c
@@ -0,0 +1,10 @@
+// https://github.com/TeleparioAlf/Programmieraufgaben/blob/main/Fruchtsaft%20herstellen.md
+// Created by lukas on 25.03.2025.
+//
+#include <ctype.h>
+#include <stdio.h>
+#include <inttypes.h>
+
+int main(void){
+
+}

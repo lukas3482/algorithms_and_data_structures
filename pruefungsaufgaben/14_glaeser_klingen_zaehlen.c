63029d4997c77bb608260f364f6d089026150864
Lukas Wolf <lukas.wolf00@gmx.at>
2025-03-27 12:12:08 +0100

    Create 14_glaeser_klingen_zaehlen.c

---
+++ pruefungsaufgaben/14_glaeser_klingen_zaehlen.c
@@ -0,0 +1,11 @@
+// https://github.com/TeleparioAlf/Programmieraufgaben/blob/main/Gl%C3%A4ser%20ansto%C3%9Fen.md
+// Created by lukas on 27.03.2025.
+//
+#include <stdio.h>
+#include <inttypes.h>
+
+int main(void){
+  int32_t n = 4;
+  int anstoesse = (n * (n - 1)) / 2;
+  printf("%d\n", anstoesse);
+}

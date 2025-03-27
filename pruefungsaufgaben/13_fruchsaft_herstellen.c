82cac0d3ef7b52dc61a4e2271342eb212027816c
Lukas Wolf <lukas.wolf00@gmx.at>
2025-03-27 12:03:29 +0100

    Update 13_fruchsaft_herstellen.c

--- pruefungsaufgaben/13_fruchsaft_herstellen.c
+++ pruefungsaufgaben/13_fruchsaft_herstellen.c
@@ -3,8 +3,47 @@
 //
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <inttypes.h>
+#include <string.h>
+
+struct Fruchtsaft {
+  char art;
+  int needed;
+};
 
 int main(void){
+  uint8_t n = 2;
+  scanf("%d", &n);
+  fgetc(stdin);
 
+  struct Fruchtsaft saefte[n];
+  for(uint8_t i = 0; i < n; i++) {
+    char c;
+    int g;
+    scanf("%c %d", &c, &g);
+    fgetc(stdin);
+
+    saefte[i].art = c;
+    saefte[i].needed = g;
+  }
+
+  char fruits[] = "FFFAFAFAFAFAFAFAAA";
+
+  uint8_t sum = 0;
+  for (uint8_t i = 0; i < n; i++) {
+    struct Fruchtsaft saft = saefte[i];
+    uint8_t length = strlen(fruits);
+    uint8_t count = 0;
+    for (uint8_t j = 0; j < length; j++) {
+      if(saft.art == fruits[j]) {
+        count++;
+        if(saft.needed == count) {
+          sum++;
+          count = 0;
+        }
+      }
+    }
+  }
+  printf("%d\n", sum);
 }

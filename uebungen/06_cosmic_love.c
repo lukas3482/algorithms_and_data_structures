e0e57d70857f85783cc9ae30192c87b814113486
Lukas Wolf <lukas.wolf00@gmx.at>
2025-04-03 12:11:57 +0200

    Create 06_cosmic_love.c

---
+++ uebungen/06_cosmic_love.c
@@ -0,0 +1,27 @@
+// https://www.codingame.com/training/easy/cosmic-love
+// Created by lukas on 03.04.2025.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++) {
+        char name[101];
+        char r[101];
+        char m[101];
+        char c[101];
+        scanf("%s%s%s%s", name, r, m, c);
+    }
+
+    // fprintf(stderr, "Debug messages...\n");
+
+    printf("answer\n");
+
+    return 0;
+}
